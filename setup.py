df1b437d533e8f4214904b10689faf4425ba54c0
yosyo <mmoussou@student.42angouleme.fr>
2024-02-16 13:56:48 +0100

    [🔨] fix(setup.py): indentation

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
- from setuptools import setup
-    setup(
-        name='pogit',
-        version='0.1.0',
-        scripts=[
-            'sg'
-        ],
-    )
+from setuptools import setup
+setup(
+    name='pogit',
+    version='0.1.0',
+    scripts=[
+        'sg'
+    ],
+)
