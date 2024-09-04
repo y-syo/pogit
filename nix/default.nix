a8b76f19940b16b38c7fb13679ca7ee0978e08c6
yosyo <mmoussou@student.42angouleme.fr>
2024-09-04 13:11:34 +0200

    「🏗️」 wip(file_checking): added error checking in the TOML file

--- nix/default.nix
+++ nix/default.nix
@@ -4,6 +4,6 @@
 }:
 python3.pkgs.buildPythonApplication {
   name = "pogit";
-  version = "0.5.0";
+  version = "0.6.1";
   src = ../.;
 }
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='pogit',
-    version='0.6.0',
+    version='0.6.1',
     scripts=[
         'pogit'
     ],
