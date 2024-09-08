cb7450318a258195ec7b1a8a5b0d4231f179836d
y-syo <mmoussou@student.42angouleme.fr>
2024-09-08 20:21:10 +0200

    「🔨」 fix: fixed missformatted line

--- nix/default.nix
+++ nix/default.nix
@@ -4,6 +4,6 @@
 }:
 python3.pkgs.buildPythonApplication {
   name = "pogit";
-  version = "0.6.1";
+  version = "0.6.2";
   src = ../.;
 }
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='pogit',
-    version='0.6.1',
+    version='0.6.2',
     scripts=[
         'pogit'
     ],
